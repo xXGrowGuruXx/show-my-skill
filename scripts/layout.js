417f2f089a459fcfa9cbad10dfc894782c685144
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-02-18 10:30:29 +0100

    update layout.js

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,9 +1,13 @@
 function fixZoom() {
     let zoomFactor = 1 / window.devicePixelRatio;
-    document.body.style.transform = `scale(${zoomFactor})`;
-    document.body.style.transformOrigin = "top left";
-    document.body.style.width = `calc(100% * ${zoomFactor})`;
-    document.body.style.height = `calc(100% * ${zoomFactor})`;
+
+    if ("zoom" in document.body.style) {
+        document.body.style.zoom = zoomFactor; // Für Chrome & Co.
+    } else {
+        document.body.style.transform = `scale(${zoomFactor})`;
+        document.body.style.transformOrigin = "top left";
+        document.documentElement.style.overflow = "hidden"; // Fix für Scrollprobleme
+    }
 }
 
 window.addEventListener("DOMContentLoaded", fixZoom);
